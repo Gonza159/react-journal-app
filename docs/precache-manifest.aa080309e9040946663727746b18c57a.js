641855e2a102e335f1b950dc64033fb0e55155a6
Gonzalo S <gonzaa159@gmail.com>
2020-08-30 21:50:56 -0300

    agregue el docs

---
+++ docs/precache-manifest.aa080309e9040946663727746b18c57a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c144af1ac85f471f1b91bc2835cffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aed22034df9077dbae6",
+    "url": "/static/css/main.26922b4e.chunk.css"
+  },
+  {
+    "revision": "5fa0f1de0c6e81f4d4e2",
+    "url": "/static/js/2.937c6752.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.937c6752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aed22034df9077dbae6",
+    "url": "/static/js/main.4fd7ff97.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
